6e631971c530d8b5fcbab8c5490906ac160c5d3c
Halil Dogan <33289@ma-web.nl>
2022-01-24 11:25:37 +0100

    commit index aangepast

--- Website/script.js
+++ Website/script.js
@@ -3,70 +3,70 @@ const foto = document.getElementById("foto");
 const myInput = document.getElementById("myInput")
 
 let lokaties = [
-{
-    "titel":"plaats 0",
-    "image":"img/0.jpg"
-},
-{
-    "titel":"plaats 1",
-    "image":"IMG_0533.JPG"
-},
-{
-    "titel":"plaats 2",
-    "image":"img/2.jpg"
-},
-{
-    "titel":"plaats 3",
-    "image":"img/3.jpg"
-},
-{
-    "titel":"plaats 4",
-    "image":"img/4.jpg"
-},
-{
-    "titel":"plaats 5",
-    "image":"img/5.jpg"
-},
-{
-    "titel":"plaats 6",
-    "image":"img/6.jpg"
-},
-{
-    "titel":"plaats 7",
-    "image":"img/7.jpg"
-},
-{
-    "titel":"plaats 8",
-    "image":"img/8.jpg"
-},
-{
-    "titel":"plaats 9",
-    "image":"img/9.jpg"
-},
-{
-    "titel":"plaats 10",
-    "image":"img/10.jpg"
-},
-{
-    "titel":"plaats 11",
-    "image":"img/11.jpg"
-},
+    {
+        "titel": "plaats 0",
+        "image": "IMG/02.jpg"
+    },
+    {
+        "titel": "plaats 1",
+        "image": "IMG/05.jpg"
+    },
+    {
+        "titel": "plaats 2",
+        "image": "IMG/03.jpg"
+    },
+    {
+        "titel": "plaats 3",
+        "image": "img/3.jpg"
+    },
+    {
+        "titel": "plaats 4",
+        "image": "img/4.jpg"
+    },
+    {
+        "titel": "plaats 5",
+        "image": "img/5.jpg"
+    },
+    {
+        "titel": "plaats 6",
+        "image": "img/6.jpg"
+    },
+    {
+        "titel": "plaats 7",
+        "image": "img/7.jpg"
+    },
+    {
+        "titel": "plaats 8",
+        "image": "img/8.jpg"
+    },
+    {
+        "titel": "plaats 9",
+        "image": "img/9.jpg"
+    },
+    {
+        "titel": "plaats 10",
+        "image": "img/10.jpg"
+    },
+    {
+        "titel": "plaats 11",
+        "image": "img/11.jpg"
+    },
 ]
 
 //myTitle.innerHTML = "dit is door het script toegevoegd";
 //myImage.src = "img/1.jpg";
 
-function show(index){
+function show(index) {
     myTitle.innerHTML = lokaties[index].titel;
     myImage.src = lokaties[index].image;
 
 }
 
-function getInput(){
+function getInput() {
     show(myInput.value);
     //console.log(myInput.value)
     myInput.value = "";
     myInput.focus();
 }
 
-show(1);
+show(0);
